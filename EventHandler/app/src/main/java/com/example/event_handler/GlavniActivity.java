62263b08b32f286b603df5e91aba3a919686fae2
Danijel <Danijel.demcic@gmail.com>
2019-07-12 17:40:54 +0200

    Mapa poctni ekran

--- EventHandler/app/src/main/java/com/example/event_handler/GlavniActivity.java
+++ EventHandler/app/src/main/java/com/example/event_handler/GlavniActivity.java
@@ -1,35 +1,59 @@
 package com.example.event_handler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import Fragments.EventsFragment;
+import Fragments.FriendsFragment;
+import Fragments.MainFragment;
 
 public class GlavniActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    private ActionBar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragment = new MainFragment();
+                    toolbar.setTitle("Main");
+                    loadFragment(fragment);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_events:
+                    fragment = new EventsFragment();
+                    toolbar.setTitle("Events");
+                    loadFragment(fragment);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_friends);
+                case R.id.navigation_friends:
+                    fragment = new FriendsFragment();
+                    toolbar.setTitle("Friends");
+                    loadFragment(fragment);
                     return true;
             }
-            return false;
+
+            return true;
         }
     };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,12 +62,49 @@ public class GlavniActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id)
+        {
+            case R.id.show_map:
+                Intent i = new Intent(this, MapActivity.class);
+                startActivity(i);
+                break;
+            case R.id.add_event:
+                break;
+            case R.id.add_friend:
+                break;
+            case R.id.show_events:
+                //TODO promeni na odgovarajuci nav item
+                toolbar.setTitle("Events");
+                loadFragment(new EventsFragment());
+                break;
+            case R.id.show_friends:
+                toolbar.setTitle("Friends");
+                loadFragment(new FriendsFragment());
+                break;
+            case R.id.change_interest:
+                break;
+            case R.id.settings:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glavni);
+
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("MAIN");
+
         BottomNavigationView navView = findViewById(R.id.nav_glavni);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(new MainFragment());
     }
 
 }
